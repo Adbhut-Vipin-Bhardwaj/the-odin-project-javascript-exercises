cc2d47949346483e34e0a4c97a6b5f7b4aeb0c36
adbhut <adbhut.bhardwaj1410@gmail.com>
2024-02-24 01:31:44 +0530

    [ADB] Do exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input_arr, ...to_be_removed) {
+    return input_arr.filter(
+        x => !to_be_removed.includes(x)
+    );
 };
 
 // Do not edit below this line
