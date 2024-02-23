78ed63019e24efc5afbfc6a9eae194be0e25511f
adbhut <adbhut.bhardwaj1410@gmail.com>
2024-02-24 01:17:01 +0530

    [ADB] Do exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input_str) {
+    let output_str = '';
+    for (let i=0; i<input_str.length; i++){
+        output_str += input_str[input_str.length - 1 - i];
+    }
+    return output_str;
 };
 
 // Do not edit below this line
