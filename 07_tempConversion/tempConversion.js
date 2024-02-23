65763b62c5189cde767bccf046622ab73f729c0b
adbhut <adbhut.bhardwaj1410@gmail.com>
2024-02-24 02:00:30 +0530

    [ADB] Do exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit_temp) {
+  let celsius_temp = (fahrenheit_temp - 32) * 5 / 9;
+  return Math.round(celsius_temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius_temp) {
+  let fahrenheit_temp = (9 * celsius_temp / 5) + 32;
+  return Math.round(fahrenheit_temp * 10) / 10;
 };
 
 // Do not edit below this line
