0ec99a26e16cf79589d261c0cab8e3caed2c12fd
adbhut <adbhut.bhardwaj1410@gmail.com>
2024-02-24 01:11:40 +0530

    [ADB] Do exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(input_str, num_repetition) {
+    if (num_repetition < 0){
+        return 'ERROR';
+    }
+    let output_str = '';
+    for (let i=0; i<num_repetition; i++){
+        output_str += input_str;
+    }
+    return output_str;
 };
 
 // Do not edit below this line
