8797b1ef46ea570ee7d185348bb5f299c403ba0b
adbhut <adbhut.bhardwaj1410@gmail.com>
2024-02-24 01:43:54 +0530

    [ADB] Do exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(bnd_1, bnd_2) {
+    if (bnd_1 < 0 || bnd_2 < 0){
+        return "ERROR";
+    }
+    if (typeof bnd_1 != 'number' || typeof bnd_2 != 'number'){
+        return "ERROR";
+    }
+    let lwr_bnd = bnd_1 < bnd_2 ? bnd_1 : bnd_2;
+    let upr_bnd = bnd_1 + bnd_2 - lwr_bnd;
+    
+    let sum = 0;
+    for (let i=lwr_bnd; i<=upr_bnd; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
